0951f3c2d6154e94872f1fee39ee15d1128a3ec7
Ayush Patel <79853695+ayush-patel-78@users.noreply.github.com>
2021-09-11 19:11:42 +0530

    getinput function code added

--- source code/car.c
+++ source code/car.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "conio2.h"
 #include "car.h"
 
@@ -14,3 +15,84 @@ void addAdmin()
     }
     fclose(fp);
 }
+
+User* getInput()
+{
+    int i;
+    clrscr();
+    textcolor(YELLOW);
+    gotoxy(32,1);
+    printf("CAR RENTAL SYSTEM\n");
+    for(i=1;i<=80;i++)
+        printf("%c",247);
+
+    gotoxy(32,5);
+    printf("* LOGIN PANEL *");
+
+    gotoxy(1,7);
+    textcolor(LIGHTCYAN);
+    for(i=1;i<=80;i++)
+        printf("%c",247);
+
+    gotoxy(1,15);
+    for(i=1;i<=80;i++)
+      printf("%c",247);
+
+    gotoxy(60,8);
+    textcolor(WHITE);
+    printf("Press 0 to exit");
+    textcolor(LIGHTCYAN);
+    gotoxy(25,10);
+    printf("Enter Userid:");
+    textcolor(WHITE);
+    static User u;
+    fflush(stdin);
+    fgets(u.userid,20,stdin);
+    char *pos;
+    pos=strchr(u.userid,'\n');
+    if(pos!=NULL)
+    {
+        *pos='\0';
+    }
+    if(strcmp(u.userid,"0")==0)
+    {
+        textcolor(LIGHTRED);
+        gotoxy(30,17);
+        printf("Login Cancelled");
+        getch();
+        textcolor(YELLOW);
+        return NULL;
+    }
+    textcolor(LIGHTCYAN);
+    gotoxy(25,11);
+    printf("Enter Password:");
+    fflush(stdin);
+    i=0;
+    textcolor(WHITE);
+    for(;;)
+    {
+        u.pwd[i]=getch();
+        if(u.pwd[i]==13)
+        {
+            break;
+        }
+        printf("*");
+        i++;
+    }
+    u.pwd[i]='\0';
+    if(strcmp(u.pwd,"0")==0)
+    {
+        textcolor(LIGHTRED);
+        gotoxy(30,17);
+        printf("Login Cancelled");
+        getch();
+        textcolor(YELLOW);
+        return NULL;
+    }
+
+    getch();
+
+
+
+}
+
